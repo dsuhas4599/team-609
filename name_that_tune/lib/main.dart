d700c8095067c3950316a50439785dad85d10296
Suhas Dundigalla <d.suhas.4599@gmail.com>
2021-02-16 22:07:55 -0500

    testing

--- name_that_tune/lib/main.dart
+++ name_that_tune/lib/main.dart
@@ -36,7 +36,7 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Welcome!", style: Theme.of(context).textTheme.headline2),
+        child: Text("Hello!", style: Theme.of(context).textTheme.headline2),
       ),
     );
   }
