7d8b7069558f10267b2a910a598fa6cc111ad05d
Grant McGrew <gdmcgrew@live.unc.edu>
2021-03-01 22:04:39 -0500

    added actual player and music video

--- name_that_tune/lib/ui/song_ui.dart
+++ name_that_tune/lib/ui/song_ui.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_starter/ui/components/components.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class SongUI extends StatelessWidget {
+  final YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: 'IC5PL0XImjw',
+    params: YoutubePlayerParams(
+      playlist: [], // Defining custom playlist
+      startAt: Duration(seconds: 30),
+      showControls: true,
+      showFullscreenButton: true,
+    ),
+  );
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: PrimaryButton(
-          labelText: "Press to play song", onPressed: () async {}),)
-    );
+        body: Center(
+            child: ListView(
+      children: <Widget>[
+        PrimaryButton(labelText: "Press to play song", onPressed: () async {}),
+        YoutubePlayerIFrame(
+          controller: _controller,
+          aspectRatio: 16 / 9,
+        ),
+      ],
+    )));
   }
 }
