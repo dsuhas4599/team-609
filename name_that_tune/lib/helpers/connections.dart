16591d54efac763a056ac4744277eac232d9125a
Suhas <d.suhas.4599@gmail.com>
2021-03-17 00:40:50 -0400

    added basic functions to write (game/round/score) to firebase

--- name_that_tune/lib/helpers/connections.dart
+++ name_that_tune/lib/helpers/connections.dart
@@ -1,10 +1,14 @@
 // Functions for getting data from firebase
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_starter/models/models.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 CollectionReference playlists = FirebaseFirestore.instance.collection('playlists');
 CollectionReference songs = FirebaseFirestore.instance.collection('songs');
+CollectionReference game = FirebaseFirestore.instance.collection('game');
+CollectionReference rounds = FirebaseFirestore.instance.collection('rounds');
+CollectionReference scores = FirebaseFirestore.instance.collection('scores');
 
 // Song functions
 Future getAllSongs() async {
@@ -47,4 +51,39 @@ Future getAllPlaylists() async {
         })
       });
   return playlistObjects;
+}
+
+//Functions to write to firestore
+Future<void> addGame(var rounds, var user) {
+  return game
+    .add({
+      'rounds': rounds, 
+      'user': user
+    })
+    .then((value) => print("game added"))
+    .catchError((error) => print("failed to add game"));
+}
+
+Future<void> addRound(int guesses, var user, var time, var song) {
+  return game
+    .add({
+      'user': user,
+      'guesses': guesses,
+      'song': song,
+      'time': time
+    })
+    .then((value) => print("round added"))
+    .catchError((error) => print("failed to add round"));
+}
+
+Future<void> addScores(var game, var user, var date, var score) {
+  return game
+    .add({
+      'game': game,
+      'user': user,
+      'date': date,
+      'score': score
+    })
+    .then((value) => print("sccore added"))
+    .catchError((error) => print("failed to add score"));
 }
